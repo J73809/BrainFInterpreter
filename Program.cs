05258e033c5563b29842a80a927e5be698961727
j7 <143880965+J73809@users.noreply.github.com>
2025-12-10 21:51:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BrainFInterpreter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? mainCode;
+        Interpreter interpreter = new Interpreter();
+        
+        Console.WriteLine("Enter the code: \n");
+        mainCode = Console.ReadLine();
+        Console.WriteLine("\nOutput: ");
+        
+        interpreter.Run(mainCode);
+    }
+}
